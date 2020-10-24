2f77a1bbb376d5ddad975d323c195f12e126e7e2
softozhik <softozhik@live.ru>
2020-10-24 16:38:29 +0300

    1. уточняющая аннотация для выбора собаки 2. класс timer тоже помечен, как бин 3. по предложению ide Autowired не для поля, а для конструктора, заодно в него и связь с бином таймера 4. в тестах используется метод, который связывает AnimalsCage c генератором времени в Timer - создал и его, время одинаковое и через поле, и через метод

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,37 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
+    private final Animal animal;
+    private final Timer timer;
+
+    // "уточняем" что животное - собака
+    // связывает бин таймер с этим классом
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
+
+    // в тестах такой метод зачем-то есть, добавил сюда
+    public Timer getTimer() {
+        return timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        //System.out.println(new Timer().getTime());
+        // заменяем создание объекта на вызов связанного бина (надеюсь, я все правильно понял)
+        System.out.println(timer.getTime());
+        //метод, который вызывается из теста
+        //System.out.println(getTimer().getTime());
         System.out.println("________________________");
     }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,8 @@
 package app.model;
 
+import org.springframework.stereotype.Component;
+
+@Component // создаем бин
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
